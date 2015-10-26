2fe45352530539182fc6a82ee142f00ee83d67ad
Chris Hu <icetime17@126.com>
2015-10-26 11:20:16 +0800

    DemoRestKit: 添加ATS和注释。

--- DemoRestKit/DemoRestKit/ViewController.m
+++ DemoRestKit/DemoRestKit/ViewController.m
@@ -29,13 +29,13 @@
     NSURL *baseURL = [NSURL URLWithString:@"http://127.0.0.1:5000"];
     AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:baseURL];
     
-    // 初始化 RestKit, 与RESTful services交互
+    // 使用AFHttpClient来初始化RKObjectManager, 与RESTful services交互
     RKObjectManager *objectManager = [[RKObjectManager alloc] initWithHTTPClient:httpClient];
     
-    // 建立 mappings, 用于配置JSON与本地Model的映射信息
+    // 建立RKObjectMappings, 用于配置JSON与本地Model的映射信息
     RKObjectMapping *articleMapping = [RKObjectMapping mappingForClass:[Article class]];
     
-    // 解析字段, 有多种方式.
+    // 解析字段, 有多种方式. 使用addAttributeMappingsFromArray等进行添加
     // [articleMapping addAttributeMappingsFromArray:@[@"title", @"author", @"body"]];
     [articleMapping addAttributeMappingsFromDictionary:@{
                                                          @"title": @"title",
@@ -45,16 +45,19 @@
     
     NSIndexSet *statusCodes = RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful); // statuscode: 2xx
     
+    // RKResponseDescriptor描述对返回数据的映射信息。
     // register mappings with the provider using a response descriptor
-    // pathPattern: API路径
+    // pathPattern: 添加到baseURL后面的API路径
     // keyPath: 对象在JSON数据中的路径
     RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:articleMapping method:RKRequestMethodGET pathPattern:@"/json" keyPath:@"article" statusCodes:[NSIndexSet indexSetWithIndex:200]];
     
     [objectManager addResponseDescriptor:responseDescriptor];
     
     
+    // 根据RKResponseDescriptor中的配置信息来映射Article对象
     NSDictionary *queryParams = nil;
-    [[RKObjectManager sharedManager] getObjectsAtPath:@"/json" parameters:queryParams success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
+    // [RKObjectManager sharedManager] 即为objectManager（单例模式）
+    [objectManager getObjectsAtPath:@"/json" parameters:queryParams success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
         _articles = mappingResult.array;
         NSLog(@"_articles.count : %ld", _articles.count);
         Article *article = [mappingResult firstObject];
