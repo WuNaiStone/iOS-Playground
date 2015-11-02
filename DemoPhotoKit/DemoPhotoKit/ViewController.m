8fafd060f20dd0e1b3abec2238f495dc00e3b616
Chris Hu <icetime17@126.com>
2015-11-02 11:06:12 +0800

    DemoPhotoKit:遍历PHAssetCollection

--- DemoPhotoKit/DemoPhotoKit/ViewController.m
+++ DemoPhotoKit/DemoPhotoKit/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    [self loadOnePhoto];
     [self loadPhotos];
 }
 
@@ -27,12 +28,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)loadPhotos {
-    // 所有智能相册
-    PHFetchResult *smartAlbums = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
-    // 所有用户创建相册
-    PHFetchResult *topLevelUserCollections = [PHCollectionList fetchTopLevelUserCollectionsWithOptions:nil];
-    
+- (void)loadOnePhoto {
     // 获取所有资源的集合，并按资源创建时间排序
     PHFetchOptions *options = [[PHFetchOptions alloc] init];
     options.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:YES]];
@@ -44,6 +40,37 @@
     [imageManager requestImageForAsset:asset targetSize:CGSizeMake(300, 200) contentMode:PHImageContentModeAspectFill options:nil resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
         NSLog(@"%@", result);
     }];
+}
+
+- (void)loadPhotos {
+    // 所有智能相册
+    PHFetchResult *smartAlbums = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    // 所有用户创建相册
+    PHFetchResult *topLevelUserCollections = [PHCollectionList fetchTopLevelUserCollectionsWithOptions:nil];
+    
+    for (NSInteger i=0; i<smartAlbums.count; i++) {
+        // 获取一个相册PHAssetCollection
+        PHCollection *collection = smartAlbums[i];
+        if ([collection isKindOfClass:[PHAssetCollection class]]) {
+            PHAssetCollection *assetCollection = (PHAssetCollection *)collection;
+            // 从一个相册中获取的PHFetchResult中包含的才是PHAsset
+            PHFetchResult *fetchResult = [PHAsset fetchAssetsInAssetCollection:assetCollection options:nil];
+            PHAsset *asset = nil;
+            if (fetchResult.count != 0) {
+                asset = fetchResult[0];
+            }
+            
+            // 使用PHImageManager从PHAsset中请求图片
+            PHImageManager *imageManager = [[PHImageManager alloc] init];
+            [imageManager requestImageForAsset:asset targetSize:PHImageManagerMaximumSize contentMode:PHImageContentModeAspectFill options:nil resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
+                if (result) {
+                    NSLog(@"%@", result);
+                }
+            }];
+        } else {
+            NSAssert1(NO, @"Fetch collection not PHCollection: %@", collection);
+        }
+    }
 }
 
 @end
