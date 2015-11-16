17e1be96816569836c947027969dd73f785ea014
Chris Hu <icetime17@126.com>
2015-11-16 20:27:38 +0800

    DemoShimmer:slide to unlock

--- DemoShimmer/DemoShimmer/ViewController.swift
+++ DemoShimmer/DemoShimmer/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let shimmerView = FBShimmeringView(frame: self.view.frame)
+        let shimmerView = FBShimmeringView(frame: CGRectMake(0, 0, self.view.frame.size.width, 50))
+        shimmerView.center = self.view.center
         self.view.addSubview(shimmerView)
         shimmerView.backgroundColor = UIColor.darkGrayColor()
         shimmerView.shimmering = true
@@ -22,9 +23,18 @@ class ViewController: UIViewController {
         let label = UILabel(frame: CGRectMake(0, 200, self.view.frame.size.width, 50))
         label.textAlignment = NSTextAlignment.Center
         label.textColor = UIColor.whiteColor()
-        label.text = "Shimmering Label"
+        label.text = ">>>  Slide to unlock"
         shimmerView.contentView = label
+        
+        let swipeGesture = UISwipeGestureRecognizer(target: self, action: Selector("unlock:"))
+        swipeGesture.direction = UISwipeGestureRecognizerDirection.Right
+        shimmerView.addGestureRecognizer(swipeGesture)
     }
+    
+    func unlock(sender: UISwipeGestureRecognizer) {
+        print(sender)
+    }
+    
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
