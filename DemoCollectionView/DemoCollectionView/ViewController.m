ca9754d004e46afb9f4b96a6f9d29636f32a3c83
Chris Hu <icetime17@126.com>
2016-01-14 07:49:56 +0800

    DemoCollectionView：补充注释。

--- DemoCollectionView/DemoCollectionView/ViewController.m
+++ DemoCollectionView/DemoCollectionView/ViewController.m
@@ -121,7 +121,7 @@
 }
 
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
-    // 设置section的insets属性。
+    // 设置section的insets属性，即边距。
     if (section == sectionCount - 1) {
         return UIEdgeInsetsMake(0, 0, 0, 0);
     }
@@ -129,12 +129,14 @@
 }
 
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    // 最小行间距
     // vertical：同一个section的上下两行（另起一行）之间的最小间隔
     // horizontal：同一个section的左右两列（另起一列）之间的最小间隔
     return 2.0;
 }
 
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    // 最小item间距
     // vertical：一行间水平（horizontal）相邻两个item之间的最小间隔
     // horizontal：一列间上下（vertical）相邻两个item之间的最小间隔
     // 而item之间实际的间隔由UIEdgeInsets及item个数共同决定（各item之间尽量保持相同间距，不够就另起一行）
