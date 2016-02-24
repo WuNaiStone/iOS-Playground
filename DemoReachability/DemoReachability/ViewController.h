5974dcbc545a74ffea55e520a29dde8cbc43c28b
Chris Hu <icetime17@126.com>
2016-02-24 10:46:45 +0800

    DemoReachability.

---
+++ DemoReachability/DemoReachability/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoReachability
+//
+//  Created by zj－db0465 on 16/2/24.
+//  Copyright © 2016年 icetime17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DemoReachability/DemoReachabilityTests/DemoReachabilityTests.m
@@ -0,0 +1,39 @@
+//
+//  DemoReachabilityTests.m
+//  DemoReachabilityTests
+//
+//  Created by zj－db0465 on 16/2/24.
+//  Copyright © 2016年 icetime17. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DemoReachabilityTests : XCTestCase
+
+@end
+
+@implementation DemoReachabilityTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
