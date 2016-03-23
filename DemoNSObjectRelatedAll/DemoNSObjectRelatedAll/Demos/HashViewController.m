6da370efe2a3db4cff5d3524caedc79c89f99e8a
Chris Hu <icetime17@126.com>
2016-03-23 18:17:53 +0800

    DemoNSObjectRelatedAll：添加Mutable对象，添加log

--- DemoNSObjectRelatedAll/DemoNSObjectRelatedAll/Demos/HashViewController.m
+++ DemoNSObjectRelatedAll/DemoNSObjectRelatedAll/Demos/HashViewController.m
@@ -96,44 +96,80 @@
     NSIndexPath *five = [NSIndexPath indexPathForRow:1 inSection:1];
     /*
      (lldb) p one
-     (NSIndexPath *) $7 = 0xc000000000000016
+     (NSIndexPath *) $1 = 0xc000000000000016
      (lldb) p two
-     (NSIndexPath *) $8 = 0xc000000000000016
+     (NSIndexPath *) $2 = 0xc000000000000016
      (lldb) p three
-     (NSIndexPath *) $9 = 0xc000000000200016
+     (NSIndexPath *) $3 = 0xc000000000200016
      (lldb) p four
-     (NSIndexPath *) $10 = 0xc000000000200016
+     (NSIndexPath *) $4 = 0xc000000000200016
      (lldb) p five
-     (NSIndexPath *) $11 = 0xc000000000200116
+     (NSIndexPath *) $5 = 0xc000000000200116
+     (lldb) p one==two
+     (bool) $6 = true
      */
     
     NSString *s1 = [[NSString alloc] initWithFormat:@"s"];
     NSString *s2 = [[NSString alloc] initWithFormat:@"s"];
-    /*
-    (lldb) p s1
-    (NSTaggedPointerString *) $1 = 0xa000000000000731 @"s"
-    (lldb) p s2
-    (NSTaggedPointerString *) $2 = 0xa000000000000731 @"s"
-     */
+    // 因此仅不可变字符串是同一内存地址，其他都是不同地址。
     
+    NSMutableString *ms1 = [[NSMutableString alloc] initWithString:@"ms"];
+    NSMutableString *ms2 = [[NSMutableString alloc] initWithString:@"ms"];
+    /*
+     (lldb) p s1
+     (NSTaggedPointerString *) $11 = 0xa000000000000731 @"s"
+     (lldb) p s2
+     (NSTaggedPointerString *) $12 = 0xa000000000000731 @"s"
+     (lldb) p ms1
+     (__NSCFString *) $13 = 0x00007fede2839e30 @"ms"
+     (lldb) p ms2
+     (__NSCFString *) $14 = 0x00007fede2805bb0 @"ms"
+     (lldb) p [ms1 isEqual:ms2]
+     (BOOL) $15 = YES
+     (lldb) p [ms1 isEqualToString:ms2]
+     (BOOL) $16 = YES
+     */
     
     NSArray *a1 = [[NSArray alloc] initWithObjects:@"1", @"2", nil];
     NSArray *a2 = [[NSArray alloc] initWithObjects:@"1", @"2", nil];
+    
+    NSMutableArray *ma1 = [[NSMutableArray alloc] initWithArray:@[@"1", @"2"]];
+    NSMutableArray *ma2 = [[NSMutableArray alloc] initWithArray:@[@"1", @"2"]];
     /*
      (lldb) p a1
-     (__NSArrayI *) $3 = 0x00007fa01bd4b5a0 @"2 objects"
+     (__NSArrayI *) $21 = 0x00007fede2830cb0 @"2 objects"
      (lldb) p a2
-     (__NSArrayI *) $4 = 0x00007fa01bd4a5e0 @"2 objects"
+     (__NSArrayI *) $22 = 0x00007fede2826ee0 @"2 objects"
+     (lldb) p ma1
+     (__NSArrayM *) $23 = 0x00007fede282e780 @"2 objects"
+     (lldb) p ma2
+     (__NSArrayM *) $24 = 0x00007fede2848100 @"2 objects"
+     (lldb) p [a1 isEqual:a2]
+     (BOOL) $25 = YES
+     (lldb) p [ma1 isEqual:ma2]
+     (BOOL) $26 = YES
      */
     
     NSDictionary *d1 = [[NSDictionary alloc] initWithObjectsAndKeys:@"value", @"key", nil];
     NSDictionary *d2 = [[NSDictionary alloc] initWithObjectsAndKeys:@"value", @"key", nil];
+    
+    NSMutableDictionary *md1 = [[NSMutableDictionary alloc] initWithObjectsAndKeys:@"value", @"key", nil];
+    NSMutableDictionary *md2 = [[NSMutableDictionary alloc] initWithObjectsAndKeys:@"value", @"key", nil];
     /*
      (lldb) p d1
-     (__NSDictionaryI *) $5 = 0x00007fa01bd45800 1 key/value pair
+     (__NSDictionaryI *) $31 = 0x00007fede2847c00 1 key/value pair
      (lldb) p d2
-     (__NSDictionaryI *) $6 = 0x00007fa01bd4da00 1 key/value pair
+     (__NSDictionaryI *) $32 = 0x00007fede2847d00 1 key/value pair
+     (lldb) p md1
+     (__NSDictionaryM *) $33 = 0x00007fede2858480 1 key/value pair
+     (lldb) p md2
+     (__NSDictionaryM *) $34 = 0x00007fede28584b0 1 key/value pair
+     (lldb) p [d1 isEqual:d2]
+     (BOOL) $35 = YES
+     (lldb) p [md1 isEqual:md2]
+     (BOOL) $36 = YES
      */
+    
     
     MyClass *cls1 = [[MyClass alloc] init];
     cls1.name = @"cls";
@@ -152,21 +188,35 @@
     /*
      NSObject的hash值暂时不清楚如何来的。
      (lldb) p s1.hash
-     (NSUInteger) $15 = 1116
+     (NSUInteger) $81 = 1116
      (lldb) p s2.hash
-     (NSUInteger) $16 = 1116
+     (NSUInteger) $82 = 1116
+     (lldb) p ms1.hash
+     (NSUInteger) $83 = 801130
+     (lldb) p ms2.hash
+     (NSUInteger) $84 = 801130
      (lldb) p a1.hash
-     (NSUInteger) $17 = 2
+     (NSUInteger) $85 = 2
      (lldb) p a2.hash
-     (NSUInteger) $18 = 2
+     (NSUInteger) $86 = 2
+     (lldb) p ma1.hash
+     (NSUInteger) $87 = 2
+     (lldb) p ma2.hash
+     (NSUInteger) $88 = 2
      (lldb) p d1.hash
-     (NSUInteger) $19 = 1
+     (NSUInteger) $89 = 1
      (lldb) p d2.hash
-     (NSUInteger) $20 = 1
+     (NSUInteger) $90 = 1
+     (lldb) p md1.hash
+     (NSUInteger) $91 = 1
+     (lldb) p md2.hash
+     (NSUInteger) $92 = 1
+     
+     
      (lldb) p cls1.hash
-     (NSUInteger) $21 = 140325638356064
+     (NSUInteger) $93 = 140659655418304
      (lldb) p cls2.hash
-     (NSUInteger) $22 = 140325638419984
+     (NSUInteger) $94 = 140659655340752
      */
 }
 
