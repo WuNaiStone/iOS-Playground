b58509430fdaab1ff3e5942a2577199c5ff660ce
Chris Hu <icetime17@126.com>
2016-04-01 12:33:35 +0800

    DemoFabric：Fabric和Crashlytics的demo，不需要key。可能在安装步骤中自动添加的。

---
+++ DemoFabric/DemoFabric/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoFabric
+//
+//  Created by Chris Hu on 16/3/24.
+//  Copyright © 2016年 icetime17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoFabric/DemoFabric/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DemoFabric
+//
+//  Created by Chris Hu on 16/3/24.
+//  Copyright © 2016年 icetime17. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
