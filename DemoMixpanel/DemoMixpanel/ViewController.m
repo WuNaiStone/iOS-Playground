272426b1e67553a498b020ec4b8f768128874390
Chris Hu <icetime17@126.com>
2016-04-03 11:31:11 +0800

    DemoOpenURL: openURL的log, 及alert的处理情况.

--- DemoMixpanel/DemoMixpanel/ViewController.m
+++ DemoMixpanel/DemoMixpanel/ViewController.m
@@ -40,6 +40,16 @@
     [btn setTitleColor:[UIColor blueColor] forState:UIControlStateHighlighted];
     [btn addTarget:self action:@selector(actionBtn:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
+    
+    
+    UIButton *btnAlert = [[UIButton alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 100, self.view.frame.size.width, 50)];
+    [btnAlert setTitle:@"Alert" forState:UIControlStateNormal];
+    [btnAlert setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [btnAlert setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
+    [btnAlert addTarget:self action:@selector(actionAlert:) forControlEvents:UIControlEventTouchUpInside];
+    btnAlert.layer.borderColor = [UIColor redColor].CGColor;
+    btnAlert.layer.borderWidth = 2.0f;
+    [self.view addSubview:btnAlert];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -68,6 +78,17 @@
     [self presentViewController:testVC animated:NO completion:nil];
 }
 
-
+- (void)actionAlert:(UIButton *)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Alert" message:@"Test OpenURL and Alert" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    [alert addAction:ok];
+    [alert addAction:cancel];
+    [self presentViewController:alert animated:NO completion:nil];
+}
 
 @end
--- DemoOpenURL/DemoOpenURL/ViewController.m
+++ DemoOpenURL/DemoOpenURL/ViewController.m
@@ -16,31 +16,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self demoOpenURL];
     
-    BOOL mixpanel = [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"demomixpanel://"]];
-    NSLog(@"mixpanel : %d", mixpanel);
-    
-    BOOL instagram = [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"instagram://"]];
-    NSLog(@"instagram : %d", instagram);
-    
+    [self demoJump];
+}
+
+- (void)demoOpenURL {
     BOOL wechat = [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"wechat://"]];
     NSLog(@"wechat : %d", wechat);
     
     BOOL weixin = [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"weixin://"]];
     NSLog(@"weixin : %d", weixin);
     
-//    NSString *username = @"icetime017";
-//    [self openUserPage:username];
-    
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"demomixpanel://"]];
+    BOOL instagram = [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"instagram://"]];
+    NSLog(@"instagram : %d", instagram);
+    NSString *username = @"icetime017";
+    [self openUserPage:username];
+    /**
+     log:
+     2016-04-03 10:46:03.718 DemoOpenURL[1474:481443] -canOpenURL: failed for URL: "instagram://" - error: "(null)"
+     2016-04-03 10:46:03.718 DemoOpenURL[1474:481443] instagram : 0
+     2016-04-03 10:46:03.719 DemoOpenURL[1474:481443] -canOpenURL: failed for URL: "instagram://location?id=1" - error: "(null)"
+     
+     未安装instagram, 则调用canOpenURL: failed for URL. 返回0
+     调用openURL, 直接跳转至instagram的网页.
+     */
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
+#pragma mark - instagram
 - (BOOL)isInstagramInstalled {
     NSURL *instagramURL = [NSURL URLWithString:@"instagram://location?id=1"];
     return [[UIApplication sharedApplication] canOpenURL:instagramURL];
@@ -53,7 +57,39 @@
     } else {
         fansPageURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://instagram.com/%@", username]];
     }
-    [[UIApplication sharedApplication] openURL:fansPageURL];
+//    [[UIApplication sharedApplication] openURL:fansPageURL];
+}
+
+#pragma mark - jump
+- (void)demoJump {
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 100, self.view.frame.size.width, 50)];
+    [btn setTitle:@"Jump to DemoMixpanel" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
+    [btn addTarget:self action:@selector(actionButton:) forControlEvents:UIControlEventTouchUpInside];
+    btn.layer.borderColor = [UIColor redColor].CGColor;
+    btn.layer.borderWidth = 2.0f;
+    [self.view addSubview:btn];
+}
+
+- (void)actionButton:(UIButton *)sender {
+    BOOL mixpanel = [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"demomixpanel://"]];
+    NSLog(@"mixpanel : %d", mixpanel);
+    
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"demomixpanel://"]];
+    
+    /**
+     未安装DemoMixpanel:
+     2016-04-03 10:50:53.433 DemoOpenURL[1480:483309] -canOpenURL: failed for URL: "demomixpanel://" - error: "(null)"
+     2016-04-03 10:50:53.434 DemoOpenURL[1480:483309] mixpanel : 0
+     调用canOpenURL: failed for URL. 返回0
+     调用openURL: 没反应.
+     
+     已安装DemoMixpanel:
+     2016-04-03 10:56:44.156 DemoOpenURL[1496:486641] mixpanel : 1
+     调用canOpenURL: 没有输出, 返回1
+     调用openURL: 跳转至DemoMixpanel.
+     */
 }
 
 @end
