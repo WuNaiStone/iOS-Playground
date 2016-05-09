069ab133a638fae7059a676ac9d2dc0aa58a0578
Chris Hu <icetime17@126.com>
2016-05-09 18:36:40 +0800

    DemoUIScrollViewZoomScale：添加UIScrollViewDelegate的更多方法。

--- DemoUIScrollViewZoomScale/DemoUIScrollViewZoomScale/ScrollViewController.m
+++ DemoUIScrollViewZoomScale/DemoUIScrollViewZoomScale/ScrollViewController.m
@@ -2,7 +2,7 @@
 //  ScrollViewController.m
 //  DemoUIScrollViewZoomScale
 //
-//  Created by zj－db0465 on 15/12/31.
+//  Created by Chris Hu on 15/12/31.
 //  Copyright © 2015年 icetime17. All rights reserved.
 //
 
@@ -52,6 +52,8 @@
 
 - (void)addScrollView {
     _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(20, 100, self.view.frame.size.width - 40, 400)];
+    _scrollView.backgroundColor = [UIColor lightGrayColor];
+    _scrollView.bounces = NO;
     _scrollView.delegate = self;
     _scrollView.minimumZoomScale = 1.f;
     _scrollView.maximumZoomScale = 10.f;
@@ -62,12 +64,18 @@
     [_scrollView addSubview:_imageView];
 }
 
+#pragma mark - <UIScrollViewDelegate>
+
 - (nullable UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
     return _imageView;
 }
 
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    NSLog(@"contentOffset : %@", NSStringFromCGPoint(scrollView.contentOffset));
+}
+
 - (void)scrollViewDidZoom:(UIScrollView *)scrollView {
-    
+    NSLog(@"zoomScale : %f", scrollView.zoomScale);
 }
 
 #pragma mark - button back
--- DemoUIScrollViewZoomScale/DemoUIScrollViewZoomScale/ViewController.m
+++ DemoUIScrollViewZoomScale/DemoUIScrollViewZoomScale/ViewController.m
@@ -2,7 +2,7 @@
 //  ViewController.m
 //  DemoUIScrollViewZoomScale
 //
-//  Created by zj－db0465 on 15/12/31.
+//  Created by Chris Hu on 15/12/31.
 //  Copyright © 2015年 icetime17. All rights reserved.
 //
 
@@ -36,7 +36,7 @@
 
 - (void)actionScrollViewController:(UIButton *)sender {
     ScrollViewController *scrollView = [[ScrollViewController alloc] init];
-    scrollView.globalZoomScale = 5.f;
+    scrollView.globalZoomScale = 1.f;
     [self presentViewController:scrollView animated:YES completion:nil];
 }
 
