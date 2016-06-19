a16ea83cd74f03fcd590a0bd6da2879535df4ad5
Chris Hu <icetime17@126.com>
2016-06-19 18:03:46 +0800

    DemoCoreData-Realm: 添加CoreData的tableView.

--- DemoCoreData-Realm/DemoCoreData-Realm/DemoCoreData/DemoPersonCoreDataViewController.m
+++ DemoCoreData-Realm/DemoCoreData-Realm/DemoCoreData/DemoPersonCoreDataViewController.m
@@ -7,19 +7,32 @@
 //
 
 #import "DemoPersonCoreDataViewController.h"
+#import "ViewTableViewCellPerson.h"
 #import "DemoPersonCoreData.h"
 #import "PersonCoreData.h"
 
-@interface DemoPersonCoreDataViewController ()
+@interface DemoPersonCoreDataViewController () <
+    UITableViewDataSource,
+    UITableViewDelegate
+>
 
 @end
 
-@implementation DemoPersonCoreDataViewController
+@implementation DemoPersonCoreDataViewController {
+
+    UITableView *_tableView;
+    
+    NSArray *_persons;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     [self demoCoreData];
+    
+    [self initTableView];
+    
+    [self updateDataSource];
 }
 
 - (void)demoCoreData {
@@ -30,6 +43,49 @@
     for (PersonCoreData *person in fetchedObjects) {
         NSLog(@"person : %@", person.description);
     }
+}
+
+- (void)initTableView {
+    _tableView = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
+    [self.view addSubview:_tableView];
+    _tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    
+    UINib *nib = [UINib nibWithNibName:NSStringFromClass([ViewTableViewCellPerson class]) bundle:nil];
+    [_tableView registerNib:nib forCellReuseIdentifier:@"UITableViewCell-CoreData"];
+    
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
+}
+
+- (void)updateDataSource {
+    _persons = [[DemoPersonCoreData sharedInstance] queryPersonCoreData];
+    
+    [_tableView reloadData];
+}
+
+#pragma mark - <UITableViewDataSource>
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _persons.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 80.f;
+}
+
+- (ViewTableViewCellPerson *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    ViewTableViewCellPerson *cell = (ViewTableViewCellPerson *)[tableView dequeueReusableCellWithIdentifier:@"UITableViewCell-CoreData" forIndexPath:indexPath];
+    
+    PersonCoreData *person     = (PersonCoreData *)_persons[indexPath.row];
+    cell.avatar.image       = [UIImage imageNamed:person.avatar];
+    cell.lbName.text        = person.name;
+    cell.lbWechatId.text    = person.wechatId;
+    cell.lbAge.text         = [NSString stringWithFormat:@"%@", person.age];
+    cell.lbCity.text        = person.city;
+    cell.lbHeight.text      = [NSString stringWithFormat:@"%@", person.height];
+    cell.lbWeight.text      = [NSString stringWithFormat:@"%@", person.weight];
+    
+    return cell;
 }
 
 @end
