57e3905428f55234c5bd441fbf7c7bce991e3ca9
Chris Hu <icetime17@126.com>
2016-10-18 13:49:30 +0800

    更新PieShapeLayer。

--- DemoCAShapeLayer/DemoCAShapeLayer/PieProgress/CSPieShapeLayer.m
+++ DemoCAShapeLayer/DemoCAShapeLayer/PieProgress/CSPieShapeLayer.m
@@ -65,7 +65,8 @@ static CGFloat offset = 2.f;
                 startAngle:0
                   endAngle:0
                  clockwise:YES];
-    [path addLineToPoint:radiusCenter];
+//    [path addLineToPoint:radiusCenter];
+    [path closePath];
     
     _shapeLayer.path = path.CGPath;
     _shapeLayer.strokeColor = [UIColor clearColor].CGColor;
@@ -83,7 +84,8 @@ static CGFloat offset = 2.f;
                 startAngle:0
                   endAngle:2 * M_PI * _progressValue
                  clockwise:YES];
-    [path addLineToPoint:radiusCenter];
+//    [path addLineToPoint:radiusCenter];
+    [path closePath];
     
     _shapeLayer.path = path.CGPath;
     [_shapeLayer setNeedsDisplay];
