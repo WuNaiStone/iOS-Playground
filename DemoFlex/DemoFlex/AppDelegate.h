19eaa6c0cb1036f1691ddc1e1e96612720b0b1ce
Chris Hu <icetime17@126.com>
2017-09-04 13:04:34 +0800

    DemoFlex: awesome debugger

---
+++ DemoFlex/DemoFlex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoFlex
+//
+//  Created by zj－db0465 on 2017/8/21.
+//  Copyright © 2017年 com.icetime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoFlex/DemoFlex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoFlex
+//
+//  Created by zj－db0465 on 2017/8/21.
+//  Copyright © 2017年 com.icetime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
